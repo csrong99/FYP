82c1804ac33ee085bd15f957a749dcf3df793373
Chuah <chuahsr99@gmail.com>
2021-01-06 00:39:54 +0800

    remove celery beat

--- video_stream/settings.py
+++ video_stream/settings.py
@@ -37,8 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'streamapp',
-    'django_celery_beat'
+    'streamapp'
 ]
 
 MIDDLEWARE = [
